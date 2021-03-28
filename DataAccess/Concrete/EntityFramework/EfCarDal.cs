bf8ba6e937eb54936ecb0e8d7c3bb319a2228e0f
Barış Aslan <baris.aslan100@gmail.com>
2021-03-29 01:42:40 +0300

    9. Gün

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -7,11 +7,32 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using Core.DataAccess.EntityFramework;
+using Entities.DTOs;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, RentCarContext>, ICarDal
     {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (RentCarContext context = new RentCarContext())
+            {
+                var result = from car in context.Cars
+                             join brand in context.Brands
+                             on car.BrandId equals brand.BrandId
 
+                             join color in context.Colors
+                             on car.ColorId equals color.ColorId
+
+                             select new CarDetailDto
+                             {
+                                 CarName = car.CarName,
+                                 BrandName = brand.BrandName,
+                                 ColorName = color.ColorName,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
