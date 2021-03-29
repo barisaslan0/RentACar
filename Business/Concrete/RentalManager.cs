11d17ca4e30deb75d2d8e83631c4f5d2d214160d
Barış Aslan <baris.aslan100@gmail.com>
2021-03-30 01:01:16 +0300

    10. Gün

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalService _rentalService;
+        public RentalManager(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+        public IResult Add(Rental rental)
+        {
+            if (rental.ReturnDate == null)
+            {
+                _rentalService.Add(rental);
+                return new SuccessResult(Messages.RentalAdded);
+            }
+
+            return new ErrorResult(Messages.CarMustReturn);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalService.Delete(rental);
+            return new SuccessResult(Messages.RentalDeleted);
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalService.Update(rental);
+            return new SuccessResult(Messages.RentalUpdated);
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,30 @@ namespace Business.Constants
         public static string CarDeleted = "Araba Başarıyla Silindi";
         public static string CarUpdated = "Araba Başarıyla Güncellendi";
         public static string CarListed = "Arabalar Listelendi";
+        public static string CarMustReturn = "Araba kiralayabilmek için arabayı teslim ediniz!";
+
+        public static string BrandAdded = "Marka Başarıyla Eklendi";
+        public static string BrandDeleted = "Marka Başarıyla Silindi";
+        public static string BrandUpdated = "Marka Başarıyla Güncellendi";
+        public static string BrandListed = "Markalar Listelendi";
+
+        public static string ColorAdded = "Renk Başarıyla Eklendi";
+        public static string ColorDeleted = "Renk Başarıyla Silindi";
+        public static string ColorUpdated = "Renk Başarıyla Güncellendi";
+        public static string ColorListed = "Renkler Listelendi";
+
+        public static string UserAdded = "Kullanıcı Başarıyla Eklendi";
+        public static string UserDeleted = "Kullanıcı Başarıyla Silindi";
+        public static string UserUpdated = "Kullanıcı Başarıyla Güncellendi";
+
+        public static string CustomerAdded = "Müşteri Başarıyla Eklendi";
+        public static string CustomerDeleted = "Müşteri Başarıyla Silindi";
+        public static string CustomerUpdated = "Müşteri Başarıyla Güncellendi";
+
+        public static string RentalAdded = "Kiralama Başarıyla Yapıldı";
+        public static string RentalDeleted = "Kiralama Başarıyla İptal Edildi";
+        public static string RentalUpdated = "Araba Başarıyla Güncellendi";
+
         public static string CarNameAndDailyPrice = "Araba ismi en az 2 harfli ve günlük değeri 0 dan büyük olmalı";
     }
 }
