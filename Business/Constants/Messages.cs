0b514556ce64db04178fdce5175ca02c5fd224f7
Barış Aslan <baris.aslan100@gmail.com>
2021-03-29 22:47:13 +0300

    10. Gün

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba Başarıyla Eklendi";
+        public static string CarDeleted = "Araba Başarıyla Silindi";
+        public static string CarUpdated = "Araba Başarıyla Güncellendi";
+        public static string CarListed = "Arabalar Listelendi";
+        public static string CarNameAndDailyPrice = "Araba ismi en az 2 harfli ve günlük değeri 0 dan büyük olmalı";
+    }
+}
