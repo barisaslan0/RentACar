9d7cbae58db7f12825897bb20b93b365147879ab
Barış Aslan <baris.aslan100@gmail.com>
2021-03-31 16:50:11 +0300

    12. Gün

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfRentalCar:EfEntityRepositoryBase<Rental,RentCarContext>,IRentalDal
+    public class EfRentalDal:EfEntityRepositoryBase<Rental,RentCarContext>,IRentalDal
     {
 
     }
